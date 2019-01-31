61aaa0a78e2449c66b26fdea49e9d3a79274c3f4
Michael <michael.zyc9@gmail.com>
2019-01-31 00:58:20 -0800

    added grid content

--- app/src/main/java/com/example/apartmentguide/adapters/GalleryAdapter.java
+++ app/src/main/java/com/example/apartmentguide/adapters/GalleryAdapter.java
@@ -5,12 +5,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.apartmentguide.R;
 import com.example.apartmentguide.models.ApartmentBuilding;
+import com.example.apartmentguide.models.FloorPlan;
 import com.squareup.picasso.Picasso;
 
 public class GalleryAdapter extends ArrayAdapter<ApartmentBuilding> {
@@ -27,6 +27,8 @@ public class GalleryAdapter extends ArrayAdapter<ApartmentBuilding> {
             convertView = LayoutInflater.from(getContext()).inflate(R.layout.grid_content, parent, false);
 
             holder.image = convertView.findViewById(R.id.grid_image);
+            holder.priceFrom = convertView.findViewById(R.id.grid_price_from);
+            holder.cityZip = convertView.findViewById(R.id.grid_city_zip);
 
             convertView.setTag(holder);
         } else {
@@ -34,11 +36,37 @@ public class GalleryAdapter extends ArrayAdapter<ApartmentBuilding> {
         }
 
         Picasso.with(getContext()).load(getItem(position).getImages()[0]).noFade().fit().into(holder.image);
+        holder.priceFrom.setText(getPriceFrom(position));
+        holder.cityZip.setText(getCityAndZip(position));
 
         return convertView;
     }
 
+    private String getCityAndZip(int position) {
+        String address = getItem(position).getAddress();
+        String[] addArr = address.split(",");
+        if(addArr.length < 2)
+            return null;
+        String cityAndZip = addArr[addArr.length - 2] + ", " + addArr[addArr.length - 1];
+        return cityAndZip;
+    }
+
+    private String getPriceFrom(int position) {
+        String priceFrom = null;
+        for (FloorPlan fp : getItem(position).getFloorPlans()) {
+            if (priceFrom == null) {
+                priceFrom = fp.getPriceFrom();
+            } else {
+                if (Integer.valueOf(fp.getPriceFrom()) < Integer.valueOf(priceFrom))
+                    priceFrom = fp.getPriceFrom();
+            }
+        }
+        return priceFrom == null ? "N/A" : ("From $" + priceFrom);
+    }
+
     private class ViewHolder{
         ImageView image;
+        TextView priceFrom;
+        TextView cityZip;
     }
 }
